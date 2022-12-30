beb38739db949b634adee248b6dec7ddf4d22e4c
denner augusto <denner.augusto90@gmail.com>
2022-12-29 21:48:51 -0300

    login final

--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,12 @@
-export function login({ email, password }) {
+export function login(email, password) {
   const delay = (0.7 + Math.random() * 2) * 1000;
 
   return new Promise((resolve, reject) => {
     setTimeout(function () {
-      if (password === 'password123' && !!email) {
-        resolve();
+      if (password === "password123" && !!email) {
+        resolve({ message: "sucesso" });
       } else {
-        reject({ message: 'e-mail or password wrong.' });
+        reject({ message: "e-mail or password wrong." });
       }
     }, delay);
   });
